4e9d6f200b5365b0514528d63728450d5efe8c2d
zhufengnodejs <zhufengnodejs@126.com>
2018-04-17 10:03:29 +0800

    1.jsx

---
+++ src/source/1.jsx.js
@@ -0,0 +1,15 @@
+//JSX表达式
+//这是一种react发明的或者说扩展的JS语法
+//这种语法浏览器是不能直接运行，如果要执行要先经过babel编译
+//babel会把这段代码 React.createElement()
+//react 核心库 react-dom这是渲染DOM相关的库
+import React from 'react';
+import ReactDOM from 'react-dom';
+let element = <h1 id="ele">12</h1>;
+//babel转议
+// var element = React.createElement(
+//     "h1",
+//     { id: "ele" },
+//     "12"
+// );
+ReactDOM.render(element,document.querySelector('#root'));
