ce9f4344199427c8a4593b57f46ec454c46e646f
zhufengnodejs <zhufengnodejs@126.com>
2018-04-17 17:16:45 +0800

    1.jsx

--- src/messages/components/MessageForm.js
+++ src/messages/components/MessageForm.js
@@ -2,7 +2,19 @@ import React,{Component} from 'react';
 export default class MessageForm extends Component{
     render(){
         return (
-            <div>MessageForm</div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="username">用户名</label>
+                    <input id="username" type="text" className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="content">内容</label>
+                    <textarea name="content" id="content" cols="30" rows="5" className="form-control"></textarea>
+                </div>
+                <div className="form-group">
+                    <input type="submit" className="btn btn-primary"/>
+                </div>
+            </form>
         )
     }
 }
