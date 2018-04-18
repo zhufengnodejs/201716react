86be2e3f90fdaddfb7008af6b99a911ce5e9a7ae
zhufengnodejs <zhufengnodejs@126.com>
2018-04-18 15:37:26 +0800

    slider

--- src/slider/components/Slider.js
+++ src/slider/components/Slider.js
@@ -1,9 +1,23 @@
 import React,{Component} from 'react';
 export default class Slider extends Component{
+    constructor(){
+        super();
+        this.state = {index:0};
+    }
+    componentDidMount(){
+        this.timerID = setInterval(()=>{
+            this.setState({index:this.state.index+1});
+        },this.props.delay*1000);
+    }
     render(){
+        let style = {
+           width:this.props.images.length*400+'px',
+           left:this.state.index*-400+'px',
+           transition:`left ${this.props.speed}s linear`
+        }
         return (
             <div className="slider-wrapper">
-                <ul className="sliders">
+                <ul className="sliders" style={style}>
                     {
                         this.props.images.map((image,index)=>(
                             <li className="slider">
