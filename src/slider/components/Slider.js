1ca9906b0f38fda6ff160be49485ebd544a18b1e
zhufengnodejs <zhufengnodejs@126.com>
2018-04-18 15:29:28 +0800

    slider

--- src/slider/components/Slider.js
+++ src/slider/components/Slider.js
@@ -2,8 +2,16 @@ import React,{Component} from 'react';
 export default class Slider extends Component{
     render(){
         return (
-            <div>
-                Slider
+            <div className="slider-wrapper">
+                <ul className="sliders">
+                    {
+                        this.props.images.map((image,index)=>(
+                            <li className="slider">
+                                <img src={image}/>
+                            </li>
+                        ))
+                    }
+                </ul>
             </div>
         )
     }
--- src/slider/index.js
+++ src/slider/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from './components/Slider';
+import './index.css'
+let images = [
+    require('./images/1.jpg'),
+    require('./images/2.jpg'),
+    require('./images/3.jpg')
+]
+/**
+ * images 所有的图片
+ * delay 每隔几秒轮播一次
+ * speed 每次轮播的时间
+ * autoPlay 是否启动自动轮播
+ * arrows 是否显示箭头
+ * dots 是否显示点状导航
+ */
 ReactDOM.render((
-    <Slider>
-
-    </Slider>
+    <Slider
+        images={images}
+        delay={3}
+        speed={1}
+        autoPlay={true}
+        arrows={true}
+        dots={true}
+    />
 ),document.querySelector('#root'));
